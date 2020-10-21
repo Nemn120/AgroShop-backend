ff3ae18cf5167120465cb1b0ff87f0728745eecd
Fernando Chavez <alvaro1221x@gmail.com>
2020-10-20 22:57:06 -0500

    basic proyect

---
+++ src/main/java/com/agroshop/app/AgroshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agroshop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroshopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agroshop/app/AgroshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroshop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
