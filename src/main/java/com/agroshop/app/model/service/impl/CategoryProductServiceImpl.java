9ff40f10237b46bd185eda448f8a005cc77fad32
JOSECONDORI5 <jose.condori5@unmsm.edu.pe>
2020-10-24 08:10:15 -0500

    crud-category-product

---
+++ src/main/java/com/agroshop/app/model/service/impl/CategoryProductServiceImpl.java
@@ -0,0 +1,36 @@
+package com.agroshop.app.model.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.agroshop.app.model.entities.CategoryProductEntity;
+import com.agroshop.app.model.repository.ICategoryProductRepository;
+import com.agroshop.app.model.service.ICategoryProductService;
+
+public class CategoryProductServiceImpl implements ICategoryProductService {
+	
+	@Autowired
+	private ICategoryProductRepository categoryRepo;
+	
+	@Override
+	public CategoryProductEntity getOneById(Integer id) {
+		return categoryRepo.getOne(id);
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		categoryRepo.deleteById(id);
+	}
+
+	@Override
+	public List<CategoryProductEntity> getAll() {
+		return categoryRepo.findAll();
+	}
+
+	@Override
+	public CategoryProductEntity save(CategoryProductEntity t) {
+		return categoryRepo.save(t);
+	}
+	
+}
