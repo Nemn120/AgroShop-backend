4b7591bffd222c5c87228dd804528309e89e0029
Gianpierre <giampier.villegas@gmail.com>
2020-10-25 11:54:17 -0500

    Configuración properties

--- src/main/java/com/agroshop/app/model/repository/IProductSalesRepository.java
+++ src/main/java/com/agroshop/app/model/repository/IProductSalesRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.agroshop.app.model.entities.ProductSalesEntity;
 
 public interface IProductSalesRepository extends JpaRepository<ProductSalesEntity, Integer> {
-	//List<ProductSalesEntity> findByIdSalesOrderByPriceAsc(int idSales);
+	// List<ProductSalesEntity> findByIdSalesOrderByPriceAsc(int idSales);
 }
