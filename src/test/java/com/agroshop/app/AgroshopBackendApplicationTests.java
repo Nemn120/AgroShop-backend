18992cf14369101ff0e0445763cf415aefa4a836
Fernando Chavez <alvaro1221x@gmail.com>
2020-11-21 18:08:08 -0500

    tst

--- src/test/java/com/agroshop/app/AgroshopBackendApplicationTests.java
+++ src/test/java/com/agroshop/app/AgroshopBackendApplicationTests.java
@@ -1,14 +1,8 @@
 package com.agroshop.app;
 
-import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 class AgroshopBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	
 }
